f62ff2e7f644cdb217fc239455b6b680520064ee
Alvaro Andres Bedoya Gomez <andresbedoyagomez@gmail.com>
2021-10-23 23:10:15 -0500

    Cambiando controller.js linea 25

--- controllers/usuarios/controller.js
+++ controllers/usuarios/controller.js
@@ -10,8 +10,7 @@ const queryAllUsers = async (callback) => {
 
 const crearUsuario = async (datosUsuario, callback) => {
   const baseDeDatos = getDB();
-  await baseDeDatos.collection('usuarios').insertOne(datosUsuario, callback);
-  
+  await baseDeDatos.collection('usuarios').insertOne(datosUsuario, callback);  
 };
 
 const consultarUsuario = async (id, callback) => {
@@ -23,7 +22,7 @@ const consultarOCrearUsuario = async (req, callback) => {
   console.log('Estoy llegando a crear usuario');
   // 6.1. obtener los datos del usuario desde el token
   const token = req.headers.authorization.split('Bearer ')[1];
-  const user = jwt_decode(token)['https://infinite-forest-27321.herokuapp.com/userData'];
+  const user = jwt_decode(token)['http://localhost/userData'];
   console.log(user);
 
   // 6.2. con el correo del usuario o con el id de auth0, verificar si el usuario ya esta en la bd o no
@@ -31,12 +30,10 @@ const consultarOCrearUsuario = async (req, callback) => {
   await baseDeDatos.collection('usuarios').findOne({ email: user.email }, async (err, response) => {
     console.log('response consulta bd', response);
     if (response) {
-      // 7.1. si el usuario ya esta en la BD, devuelve la info del usuario
-      console.log('Esta creado');
+      console.log('Esta creado');// 7.1. si el usuario ya esta en la BD, devuelve la info del usuario
       callback(err, response);
     } else {
-      console.log('Deberìa crearlo');
-      // 7.2. si el usuario no esta en la bd, lo crea y devuelve la info
+      console.log('Deberìa crearlo'); // 7.2. si el usuario no esta en la bd, lo crea y devuelve la info
       user.auth0ID = user._id;
       delete user._id;
       user.rol = 'sin rol';
